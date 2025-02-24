b7d22bad8ee6bdbb246080339004aaf3692e7e7f
jessesillman <jesse.sillman@yahoo.com>
2025-02-24 15:37:54 +0200

    Projects added + fixes

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
   res.send('Server is running.');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
                         <Card.Body>
                             <Card.Text>
                             This project involved developing a scalable weather station using an ESP32 microcontroller for real-time data collection. It includes a Go-based API, an MQTT broker, 
-                            and InfluxDB-Grafana for data storage and visualization.  The system is automated with a single startup script for easy deployment.
+                            and InfluxDB-Grafana for data storage and visualization. The system is automated with a single startup script for easy deployment.
                             </Card.Text>
                             < br />
                             <div style={{ display: 'flex', overflowX: 'auto' }}>
@@ -56,6 +56,10 @@ const Projects = () => {
                                     onClick={() => handleShow('/grafana-example.jpeg')}
                                 />
                             </div>
+                            < br />
+                            <Card.Text>
+                            <a href="https://github.com/martimolanes/Iot-Stuff" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            </Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -134,6 +138,130 @@ const Projects = () => {
                                     style={{ maxHeight: '200px', maxWidth: '700px', marginRight: '10px', cursor: 'pointer' }}
                                     onClick={() => handleShow('/taskmanager4.png')}
                                 />
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <Card className="mb-3">
+                        <Card.Header style={{ fontWeight: 'bold' }}>Fitness Tracker Application with Tkinter Python</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                This project involved creating a fitness tracker application using Tkinter in Python. The application allows users to track their workouts, calculate
+                                calories burned and bmi, and set a meal plan. The aim was to get acquainted with GUI programming in Python.
+
+
+                            </Card.Text>
+                            < br />
+                            <div style={{ display: 'flex', overflowX: 'auto' }}>
+                                <img
+                                    src="/ossmain.png"
+                                    alt="Fitness Tracker 1"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/ossmain.png')}
+                                />
+                                <img
+                                    src="/osscalories.png"
+                                    alt="Fitness Tracker 2"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/osscalories.png')}
+                                />
+                                <img
+                                    src="/ossworkout.png"
+                                    alt="Fitness Tracker 3"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/taskmanager3.png')}
+                                />
+                                <img
+                                    src="/bmicalculator.png"
+                                    alt="Fitness Tracker 4"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/bmicalculator.png')}
+                                />
+                                <img
+                                    src="/dietplan.png"
+                                    alt="Fitness Tracker 5"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/dietplan.png')}
+                                />
+                                <img
+                                    src="/tutorials.png"
+                                    alt="Fitness Tracker 6"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/tutorials.png')}
+                                />
+                            </div>
+                            < br />
+                            <Card.Text>
+                            <a href="https://github.com/jessesillman/OSSProject" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <Card className="mb-3">
+                        <Card.Header style={{ fontWeight: 'bold' }}>Simple mobile app projects using Java on Android Studio</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                These projects involved creating simple mobile applications using Java on Android Studio. The applications include a simple calculator with intent operations, image viewer, and a media player. The aim was to get acquainted
+                                with android development and Java programming.
+
+
+                            </Card.Text>
+                            < br />
+                            <div style={{ display: 'flex', overflowX: 'auto' }}>
+                                <img
+                                    src="/imageviewer.png"
+                                    alt="Image Viewer"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/imageviewer.png')}
+                                />
+                                <img
+                                    src="/mediaplayer.png"
+                                    alt="Media Player"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/mediaplayer.png')}
+                                />
+                                <img
+                                    src="/intent.png"
+                                    alt="Intent"
+                                    style={{ maxHeight: '200px', marginRight: '10px', cursor: 'pointer' }}
+                                    onClick={() => handleShow('/intent.png')}
+                                />
+                            </div>
+                            < br />
+                            <Card.Text>
+                                GitHub: 
+                                <a href="https://github.com/jessesillman/IntentJava" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>IntentJava</a> | 
+                                <a href="https://github.com/jessesillman/ImageViewerJava" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>ImageViewerJava</a> | 
+                                <a href="https://github.com/jessesillman/MediaPlayerJava" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>MediaPlayerJava</a>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <Card className="mb-3">
+                        <Card.Header style={{ fontWeight: 'bold' }}>The demonstration of GitLab CI/CD and Azure with container images</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                This video demonstrates how to set up a GitLab CI/CD pipeline to build and push a container image to the Azure Container Registry. 
+                            </Card.Text>
+                            < br />
+                            <div className="video-container" style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%', background: '#000', marginBottom: '10px' }}>
+                                <iframe
+                                    src="https://www.youtube.com/embed/M_MH6tnjRAU"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                    title="GitLab CI/CD and Azure Demonstration"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                                ></iframe>
                             </div>
                         </Card.Body>
                     </Card>
